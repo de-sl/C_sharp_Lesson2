12a20728eafa6563ca21cd32e19b4ed37064847a
de-sl <en060988@gmail.com>
2023-09-24 12:54:44 +0300

    done q13

---
+++ q13/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+void ThirdNumber(int num){
+    int trnum = num%10;
+    if (num<99)
+        Console.WriteLine("There is no third number");
+    else
+        Console.WriteLine($"The third number is {trnum}");
+}
+Console.WriteLine("Enter your number");
+int number = Convert.ToInt32(Console.ReadLine());
+ThirdNumber(number);
