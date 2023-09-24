fe0002b6bdf3479c3d5ad162704a49ebcf978332
de-sl <en060988@gmail.com>
2023-09-24 12:15:15 +0300

    done q10

---
+++ q10/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+
+void SecondNum(int num){
+    if (num >= 100 && num<1000){
+        int secnum = num/10;
+        int secnum2 = secnum%10;
+        Console.WriteLine($"Second number {secnum2}.");
+    }
+    else
+    {
+       Console.WriteLine($"Incorrect number entered.");
+    }
+}
+Console.WriteLine("Enter a number from 100 to 999");
+int number = Convert.ToInt32(Console.ReadLine());
+SecondNum(number);
