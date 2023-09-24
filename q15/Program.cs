1020d9202ec80c0290db04a72068712af95eb96c
de-sl <en060988@gmail.com>
2023-09-24 13:38:02 +0300

    done q15

---
+++ q15/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+void WeekDay(int num){
+        if (num < 6 && num > 0)
+            Console.WriteLine("It's a weekday");
+
+        if (num < 8 && num > 5)
+            Console.WriteLine("It's a weekend");
+        if (num < 0 | num > 8)
+            Console.WriteLine("It's not a day of the week");    
+}
+Console.WriteLine("Enter the serial number of the day of the week 1 to 7");
+int number = Convert.ToInt32(Console.ReadLine());
+WeekDay(number);
